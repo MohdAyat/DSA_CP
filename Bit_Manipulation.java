96e1c1f55193dbb08d16285c21ef72a6233dd34d
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-13 19:21:22 +0530

    Update and rename Bit Manipulation to Bit_Manipulation.java

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -1,3 +1,4 @@
+public class Bit_Manipulation{
 public static int[] bitManipulation(int num, int i){
         //get bit
         int arr[] = new int[3];
@@ -25,3 +26,4 @@ Bit at the 3rd position from LSB is 0. (1 1 0 0 1)
 The value of the given number after setting the 3rd bit is 29. (1 1 1 0 1)
 
 The value of the given number after clearing the 3rd bit is 25. (1 1 0 0 1)*/
+}
